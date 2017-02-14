d61e1df6bf4363f43b81e50d11933bd98893e567
stidiovip <stidiovip@yahoo.fr>
2017-02-14 11:08:41 +0100

    Hesperides version naming

--- src/app/hesperides/hesperides.js
+++ src/app/hesperides/hesperides.js
@@ -56,7 +56,7 @@ var hesperidesModule = angular.module('hesperides', [
     weekdays: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
     calendar: 'Calendrier'
 }).value('hesperidesGlobals', {
-    versionName: '${releaseName}',
+    versionName: 'CHAOS', //'${releaseName}',
     eventPaginationSize: 25
 });
 
