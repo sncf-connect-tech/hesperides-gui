a5e7ba5cbba60c6f8608b393ac57729f7ee9dcdf
emeric_martineau <emartineau@voyages-sncf.com>
2017-02-28 16:25:50 +0100

    Update docket image to hesperides 2.2.2

--- docker/docker-prebuild.sh
+++ docker/docker-prebuild.sh
@@ -3,14 +3,14 @@
 set -x
 dir="$(dirname $0)"
 cwd=$(cd $dir; pwd)
-VERSION=1.0.0
+VERSION=2.2.2
 GROUP=com.vsct.dt
 ARTIFACT=hesperides
 REPO=dt-releases
 curl -o $cwd/app.jar -J -L http://nexus/service/local/artifact/maven/redirect?r=$REPO\&g=$GROUP\&a=$ARTIFACT\&v=$VERSION\&p=jar
  
   
-VERSION=1.0.2
+VERSION=2.2.2
 GROUP=com.vsct.dt
 ARTIFACT=hesperides-gui
 REPO=dt-releases
