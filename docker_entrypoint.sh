4bef17949082db39341ad8eec7695aa272cb1212
Lucas Cimon <lucas.cimon@gmail.com>
2019-05-24 10:28:22 +0200

    chores: Ajout support $PROXY_TIMEOUT_IN_SECS dans docker_entrypoint.sh (#242)

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -o pipefail -o errexit -o nounset
 
-# Inject API_ENDPOINT env variable in hesperides.conf
+if [ -n "${PROXY_TIMEOUT_IN_SECS:-}" ]; then
+    export PROXY_TIMEOUT_DIRECTIVES="proxy_connect_timeout ${PROXY_TIMEOUT_IN_SECS}s; proxy_send_timeout ${PROXY_TIMEOUT_IN_SECS}s; proxy_read_timeout ${PROXY_TIMEOUT_IN_SECS}s; send_timeout ${PROXY_TIMEOUT_IN_SECS}s;"
+fi
+# Inject $API_ENDPOINT & $PROXY_TIMEOUT_DIRECTIVES env variables in hesperides.conf
 envsubst < /etc/nginx/conf.d/hesperides.conf.template > /etc/nginx/conf.d/hesperides.conf
 
 if [ $# -gt 0 ]; then
