7f5c88aea3cc2e54bad3269a723faadf9e94d095
Bhoye05 <barrybhoye05@gmail.com>
2019-10-09 09:39:53 +0200

    Fix #502: Valorisation conservées lors d'un rollback de version corrections après revue

--- src/app/properties/properties.js
+++ src/app/properties/properties.js
@@ -1212,7 +1212,7 @@ angular.module('hesperides.properties', [ 'hesperides.diff', 'hesperides.localCh
                 link(scope) {
                     scope.propertiesKeyFilter = '';
                     scope.propertiesValueFilter = '';
-                    scope.isSwitchChanged = false;
+                    scope.isSwitchOnlyRequirePropertiesChanged = false;
 
                     scope.hasLocalChanges = function () {
                         return LocalChanges.hasLocalChanges(scope.platform.application_name, scope.platform.name, scope.module ? scope.module.properties_path : '');
@@ -1234,11 +1234,8 @@ angular.module('hesperides.properties', [ 'hesperides.diff', 'hesperides.localCh
                         scope.properties = LocalChanges.tagWithLocalProperties(scope.platform.application_nam, scope.platform.name, scope.module.properties_path, { 'key_value_properties': scope.properties }).key_value_properties;
                     };
 
-                    scope.shownOlyRequiredProperties = function(param) { 
-                       if(scope.isSwitchChanged && !param) {
-                           return true;
-                       }
-                       return false;
+                    scope.shownOlyRequiredProperties = function(param) {                       
+                       return scope.onlyRequiredPropertiesSwitchActivated && !param;
                     };
                 },
             };
