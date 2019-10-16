930912bddb40d9ae2ccda59cc43607b8fa7a9c14
Bhoye05 <barrybhoye05@gmail.com>
2019-10-16 15:49:37 +0200

    Issue #782 Fournir la liste des propriétés d'une plateforme avec : valeur par défaut

--- src/app/module/properties-list/properties-list.js
+++ src/app/module/properties-list/properties-list.js
@@ -1,7 +1,17 @@
 angular.module('hesperides.module.propertiesList', [ 'hesperides.localChanges', 'cgNotify', 'hesperides.properties' ])
-    .controller('PropertiesListController', function ($filter, $scope, $routeParams, $timeout, $route, $q, 
-        ApplicationService, ModuleService, $translate, HesperidesModalFactory, Platform, Properties, notify) {
+    .controller('PropertiesListController', function ($scope,  $mdDialog) {
 
-
+            $scope.show_dialog_properties_list = function(providedPlatform) {
+                var modalScope = $scope.$new();
+                console.log(providedPlatform);
+                modalScope.platform=providedPlatform;
+    
+                $mdDialog.show({
+                    templateUrl: 'module/properties-list/list-properties-modal.html',
+                    controller: 'PropertiesListController',
+                    clickOutsideToClose: true,               
+                    scope: modalScope,
+                });
+            }
 
     })
