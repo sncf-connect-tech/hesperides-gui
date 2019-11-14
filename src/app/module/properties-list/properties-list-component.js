a10f05306ecbf04356078b9caed0c6ea0ed94d00
Bhoye05 <barrybhoye05@gmail.com>
2019-11-14 17:36:41 +0100

    Fournir la liste des propriétés d'une plateforme avec : valeur par défaut, valeur saisie et valeur finale #782

--- src/app/module/properties-list/properties-list-component.js
+++ src/app/module/properties-list/properties-list-component.js
@@ -9,6 +9,7 @@ function ($scope, $q, $mdDialog, ModuleService, ApplicationService) {
   $scope.properties = null;
   $scope.oldGlobalProperties = null;
   $scope.onlyPropertiesWithBlankFinalValue = false;
+  $scope.modulesProperties =[];
 
   $scope.getNbUsageOfGlobalProperty = function (property) {
       if ($scope.platform.global_properties_usage && property.valuedByAGlobal) {
@@ -19,27 +20,40 @@ function ($scope, $q, $mdDialog, ModuleService, ApplicationService) {
       }
   };
 
-  $scope.displayOnlyPropertiesWithBlankFinalValue = function(property) {
-      var isBlank = false;
-      if(property.finalValue==="" || property.finalValue===null) {
-          isBlank = true;
-      }
-      return $scope.onlyPropertiesWithBlankFinalValue && !isBlank;
+  $scope.getNbModuleDefinitionsOfProperty = function(property) {
+    property.nbUsage = 0;
+    property.modulesWhereDefine = [];
+    $scope.properties.forEach(function(prop) {
+        if(property.name ===prop.name) {
+            property.nbUsage +=1;
+            property.modulesWhereDefine.push();
+        }
+    }) 
   }
+
+  $scope.propertyFilter = function (property) {  
+    displayAllProperties = true;     
+    if($scope.onlyPropertiesWithBlankFinalValue) {
+        displayAllProperties = (property.finalValue==="" || property.finalValue===null);
+    }
+    return displayAllProperties;
+  };
 
   $scope.isGlobalValuationIsDiffrentWithModuleValuation = function(property) {
       return property.storedValue !== property.finalValue
   } 
 
   if ($scope.platform.modules && $scope.platform.modules.length) {
-      console.log('propertiesListController $scope.platform=', $scope.platform);   
-       
+           
       const propertyModelsPromises = [];
       const propertiesModulesPromies = [];
       for (const module of $scope.platform.modules) {
           propertyModelsPromises.push(ModuleService.get_model(module));
           propertiesModulesPromies.push(ApplicationService.get_properties($scope.platform.application_name, $scope.platform.name, module.properties_path, {withDetails: true}))
-      }
+          propertiesModulesPromies.forEach(function(propertyPromise) {
+            propertyPromise.moduleName = module.name;
+          })
+        }
       
       $q.all({
           globalProperties: ApplicationService.get_properties($scope.platform.application_name, $scope.platform.name, '#'),
@@ -47,21 +61,26 @@ function ($scope, $q, $mdDialog, ModuleService, ApplicationService) {
           modulePropertyModels: propertyModelsPromises,
           moduleProperties: propertiesModulesPromies
       }).then(({ globalProperties, globalPropertyUsages, modulePropertyModels, moduleProperties }) => {
-                  
-          console.log('Properties :', moduleProperties);
-          console.log('Global properties:', globalProperties);
           moduleProperties.forEach(function (modulePropery) {
-              modulePropery.then( function (properties) {
-                  $scope.properties = properties.mergeWithGlobalProperties(globalProperties); 
+              console.log("Module Properties : " , modulePropery);
+              modulePropery.then( function (properties) {                 
+                  $scope.properties = properties.mergeWithGlobalProperties(globalProperties);    
+                  newProperties = properties.mergeWithGlobalProperties(globalProperties);
+                  newProperties.moduleName = modulePropery.moduleName;             
+                  $scope.properties.moduleName = modulePropery.moduleName;  
+                                                
                   modulePropertyModels.forEach(function (model) {                
                       model.then(function(modelProperties) {
-                          $scope.properties = properties.mergeWithModel(modelProperties);                           
+                          $scope.properties = properties.mergeWithModel(modelProperties); 
+                          newProperties = properties.mergeWithModel(modelProperties); 
+                          $scope.modulesProperties.push(newProperties);                          
                           $scope.platform.global_properties = globalProperties;
-                          $scope.platform.global_properties_usage = globalPropertyUsages;    
-                          console.log('Properties :', $scope.properties);
+                          $scope.platform.global_properties_usage = globalPropertyUsages;                    
                           for (const property of $scope.properties.key_value_properties) {
                               $scope.getNbUsageOfGlobalProperty(property);
-                          }           
+                          }   
+                          
+                          console.log("ALL PROPERTIES : ",   $scope.modulesProperties);        
                       });                           
                   });                                   
               });
