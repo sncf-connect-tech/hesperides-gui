04d240c8c57a36e0cf67acd93093d7640692d530
Thomas L'Hostis <thomas.lhostis@gmail.com>
2020-01-24 11:12:59 +0100

    wip

--- .travis/run_bdd_test.sh
+++ .travis/run_bdd_test.sh
@@ -6,4 +6,4 @@ sudo env "PATH=$PATH" npm run webdriver-start --verbose &
 docker run --rm -p 8080:8080 -e SPRING_PROFILES_ACTIVE=noldap,fake_mongo hesperides/hesperides:latest &
 sudo env "PATH=$PATH" npm start &
 wget --waitretry=5 --retry-connrefused -T 60 -O - http://localhost:8080/rest/versions
-npm run bdd-tests -- --specs=test/bdd/features/$1
+npm run bdd-tests --strict --specs=test/bdd/features/$1
