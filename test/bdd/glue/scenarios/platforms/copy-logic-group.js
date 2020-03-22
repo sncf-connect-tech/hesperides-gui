23c512c12c3eee1a15d04ac3d2a0a90a91ec4df4
Thomas L'Hostis <thomas.lhostis@gmail.com>
2020-03-22 12:20:08 +0100

    Nettoyage

--- test/bdd/glue/scenarios/platforms/copy-logic-group.js
+++ test/bdd/glue/scenarios/platforms/copy-logic-group.js
@@ -59,32 +59,3 @@ Then(/^the logical group copy modal is( not)? closed$/, async function (isNotClo
         await assert.isNotPresentById(modalId);
     }
 });
-//
-// When(/^I copy this logic group "([^"]*)" to the existing logic group "([^"]*)"( with the same module name)?$/, async function (logicGroupSource, logicGroupDestination, withTheSameModuleName) {
-//     await send.mouseOnById(`e2e-tree-renderer-edit-logic-group-${ logicGroupSource }`);
-//     await send.clickById(`e2e-tree-renderer-copy-box-button-${ logicGroupSource }`);
-//     await send.clickById('e2e-copy-logical-group-to-existing-one-option');
-//     await send.mouseOnById(`e2e-logic-group-to-select-${ logicGroupDestination }`);
-//     await send.clickById(`e2e-logic-group-to-select-${ logicGroupDestination }`);
-//     if (withTheSameModuleName) {
-//         await send.clickByIdAndAcceptAlert('e2e-saveButton');
-//     } else {
-//         await send.clickById('e2e-saveButton');
-//     }
-// });
-//
-// When('I copy this logic group {string} to a new logic group {string}', async function (logicGroupSource, newLogicGroup) {
-//     await send.mouseOnById(`e2e-tree-renderer-edit-logic-group-${ logicGroupSource }`);
-//     await send.clickById(`e2e-tree-renderer-copy-box-button-${ logicGroupSource }`);
-//     await send.clickById('e2e-copy-logical-group-to-new-one-option');
-//     await send.inputById('e2e-copy-to-new-logic-group-input', newLogicGroup);
-//     await send.clickById('e2e-saveButton');
-// });
-//
-// Then('the deployed module {string} of logic group {string} are successful copied to logic group {string}', /** @this CustomWorld */ async function (module, logicGroupSource, logicGroupDestination) {
-//     await assert.isPresentById(`e2e-tree-renderer-edit-module-button-${ logicGroupDestination }-${ module }-${ this.moduleBuilder.version }`);
-// });
-//
-// Then('the deployed module {string} with version {string} is successful copied to logic group {string}', async function (module, version, logicGroupDestination) {
-//     await assert.isPresentById(`e2e-tree-renderer-edit-module-button-${ logicGroupDestination }-${ module }-${ version }`);
-// });
