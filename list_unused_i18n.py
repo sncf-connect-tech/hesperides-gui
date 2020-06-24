2519d0598e33b590d098eb0e7e8d06906352f7dd
Lucas Cimon <lucas.cimon@gmail.com>
2020-06-24 12:24:43 +0200

    Fixing pre-commit CI step

--- list_unused_i18n.py
+++ list_unused_i18n.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
+'''
+Checks that labels defined in label*.json files are all used somewhere, in order to delete useless ones.
+'''
 
-# Checks that labels defined in label*.json files are all used somewhere, in order to delete useless ones.
-# USAGE: ./list_unused_i18n.py [--remove-from-jsons]
-
-import io, json, sys
+import argparse, io, json, sys
 from glob import glob
-from os import path, walk
+from os.path import dirname, join, realpath
+from subprocess import check_output
 
 
-STATICS_SRC_DIR = path.join(path.dirname(path.realpath(__file__)), 'src', 'app')
+STATICS_SRC_DIR = join(dirname(realpath(__file__)), 'src', 'app')
 TARGET_FILES_EXTENSIONS = ('.html', '.js')
-I18N_JSON_FILEPATHS_PATTERN = path.join(STATICS_SRC_DIR, 'i18n', 'label*.json')
-# Labels to ignore. Sometimes useful, with generated labels for example.
-# If I do "'someCssClass.' + filterFamily" in my source code, this script will not see it.
+I18N_JSON_FILEPATHS_PATTERN = join(STATICS_SRC_DIR, 'i18n', 'label*.json')
+# Labels to ignore. Sometimes useful, with generated labels for example,
+# as if I write "'someCssClass.' + filterFamily" in some JS file, this script will ignore it.
 I18N_STRINGS_TO_IGNORE = ()
 
 
 def main():
-    files_to_inspect = list(scandir(STATICS_SRC_DIR, TARGET_FILES_EXTENSIONS))
+    args = parse_args()
+    files_to_inspect = check_output(['git', 'ls-files', STATICS_SRC_DIR]).decode().split()
+    files_to_inspect= [f for f in files_to_inspect if any(f.endswith(ext) for ext in TARGET_FILES_EXTENSIONS)]
     i18n_strings = set()
     for i18n_json_path in glob(I18N_JSON_FILEPATHS_PATTERN):
         with open(i18n_json_path) as i18n_file:
             i18n_strings.update(json.load(i18n_file).keys())
     i18n_strings -= set(I18N_STRINGS_TO_IGNORE)
-    unused_i18n_strings = [i18n_string for i18n_string in i18n_strings if not find_usage(i18n_string, files_to_inspect)]
+    unused_i18n_strings = set(string for string in i18n_strings
+                              if not find_usage(string, files_to_inspect, args.verbose))
     for unused_i18n_string in sorted(unused_i18n_strings):
         print('No usage found for', unused_i18n_string)
     if unused_i18n_strings:
-        if len(sys.argv) <= 1 or sys.argv[1] != '--remove-from-jsons':
+        if not args.remove_from_jsons:
             sys.exit(1)
         for i18n_json_path in glob(I18N_JSON_FILEPATHS_PATTERN):
             with open(i18n_json_path) as i18n_file:
@@ -38,17 +42,21 @@ def main():
             with open(i18n_json_path, 'w') as i18n_file:
                 json.dump(i18n, i18n_file, sort_keys=True, indent=2)
 
-def scandir(dirname, extensions):
-    for dirpath, _, fnames in walk(dirname):
-        for file_name in [path.join(dirpath, f) for f in fnames if any(f.endswith(ext) for ext in extensions)]:
-            yield file_name
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--remove-from-jsons', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('files', nargs='*')  # ignored but needed in order to be used as a pre-commit hook
+    return parser.parse_args()
 
-def find_usage(string, files_to_inspect):
+def find_usage(string, files_to_inspect, verbose):
     for file_name in files_to_inspect:
         with io.open(file_name, 'r', encoding='utf-8') as open_file:
             try:
                 for line in open_file.readlines():
                     if string in line:
+                        if verbose:
+                            print('String "{}" found in {}'.format(string, filename))
                         return True
             except:
                 print('While processing', file_name)
