f89535269c77670823da4c5e13ea66ed0a79c74b
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2021-05-26 08:46:54 +0200

    Généralisation du check des fichiers CSS & JS dans l'entrypoint Docker (#435)

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -o pipefail -o errexit -o nounset
 
 : "${API_ENDPOINT:?'This environment variable is required'}"
@@ -9,17 +9,26 @@ if [ -n "${PROXY_TIMEOUT_IN_SECS:-}" ]; then
   export PROXY_TIMEOUT_DIRECTIVES="proxy_connect_timeout ${PROXY_TIMEOUT_IN_SECS}s; proxy_send_timeout ${PROXY_TIMEOUT_IN_SECS}s; proxy_read_timeout ${PROXY_TIMEOUT_IN_SECS}s; send_timeout ${PROXY_TIMEOUT_IN_SECS}s;"
 fi
 
-# Ensure the CSS & JS dependencies files are present:
-if ! [ -s /usr/share/nginx/html/vendor.css ] || ! [ -s /usr/share/nginx/html/vendor.js ]; then
-    echo 'At least one of vendor.css / vendor.js are missing or empty' >&2
-    exit 1
-fi
-
 # Inject env variables in hesperides.conf:
 envsubst </etc/nginx/conf.d/hesperides.conf.template >/etc/nginx/conf.d/hesperides.conf
 
 # Inject env variables in index.html:
 envsubst </usr/share/nginx/html/index.html.template >/usr/share/nginx/html/index.html
+
+# Ensure all the required JS files are present:
+grep -F '<script ' /usr/share/nginx/html/index.html | sed 's/.*src="\([^"]*\)".*/\1/' | while read js_file; do
+    if ! [ -s /usr/share/nginx/html/$js_file ]; then
+        echo "$js_file is missing or empty"
+        exit 1
+    fi
+done
+# Ensure all the required CSS files are present:
+grep -F '<link rel="stylesheet"' /usr/share/nginx/html/index.html | sed 's/.*href="\([^"]*\)".*/\1/' | while read css_file; do
+    if ! [ -s /usr/share/nginx/html/$css_file ]; then
+        echo "$css_file is missing or empty"
+        exit 1
+    fi
+done
 
 if [ $# -gt 0 ]; then
   exec "$@"
